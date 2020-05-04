8d9cad00f1440db6e80fad380edb32f6cfc565f3
zivww <ziv.wang.tw@gmail.com>
2020-05-04 23:10:40 +0800

    [SystemDesign] notitfication design v1

---
+++ Interview/NotificationSystem.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************
+* Notification System
+* 
+* Database: Y/N ?
+*
+* Data Scheme: userID, ..
+*
+* Scenario:
+*
+* Cache: Y/N ?
+*
+* Message Queue:
+* 
+*
+* APIs:
+*
+*
+* OOD: Observer
+* 
+*
+* Reference: 
+*  1. https://www.youtube.com/watch?v=_63IA-Ly0gY
+*  2. https://www.youtube.com/watch?v=bBTPZ9NdSk8
+*  3. https://leetcode.com/discuss/interview-question/object-oriented-design/488544/OOD-Notification-System
+*  4. https://stackoverflow.com/questions/9735578/building-a-notification-system
+*
+*
+* Key:
+*  1. 
+*
+*
+*******************************************************************/
+
+
+
--- README.md
+++ README.md
@@ -6,4 +6,6 @@ https://www.educative.io/courses/grokking-the-system-design-interview
   
 https://github.com/justinamiller/SoftwareArchitect/blob/master/README.md  
   
+https://github.com/yangshun/tech-interview-handbook/tree/master/experimental/design  
+  
 https://leetcode.com/discuss/general-discussion/125038/System-Design-Interview-Prep/  
