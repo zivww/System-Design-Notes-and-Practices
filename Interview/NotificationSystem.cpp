49a94c79c2226a3b8ea3dceed2a43e7f92e6f458
zivww <ziv.wang.tw@gmail.com>
2020-05-21 19:38:43 +0800

    [SystemDesign] qt example

--- Interview/NotificationSystem.cpp
+++ Interview/NotificationSystem.cpp
@@ -23,6 +23,7 @@
 *  2. https://www.youtube.com/watch?v=bBTPZ9NdSk8
 *  3. https://leetcode.com/discuss/interview-question/object-oriented-design/488544/OOD-Notification-System
 *  4. https://stackoverflow.com/questions/9735578/building-a-notification-system
+*  5. GT: https://github.com/aldonin/Notification-System
 *
 *
 * Key:
